a5908fcf089eed63650143a2a8161c8b070853c9
Aryan <aryanyk562@gmail.com>
2025-02-04 01:35:01 +0530

    Added Bubble Sort

--- patterns/triangle.java
+++ patterns/triangle.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class triangle{
 
     static void pattern(int r , int c){
@@ -30,9 +33,29 @@ public class triangle{
             
         }
     }
+
+    static void bubble(int[] arr ,int r , int c){
+        if(r==0){
+            return ;
+        }
+        if(c <r){
+            if(arr[c]>arr[c+1]){
+                int temp = arr[c];
+                arr[c] =arr[c+1];
+                arr[c+1] = temp;
+            }
+            bubble(arr, r, c+1);
+        }else{
+            bubble(arr, r-1, 0);
+        }
+    }
     public static void main(String[] args) {
         pattern(5,0);
         pattern2(5,0);
+        int[] arr ={1,5,3,6,2};
+        bubble(arr, arr.length-1, 0);
+        System.out.println(Arrays.toString(arr));
+        
     }
 
     
